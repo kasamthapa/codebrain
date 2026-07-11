e1346262b8ac95af6bbf171732e0901ee64fa98f
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-07-11 20:58:45 +0545

    add cors to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
-
+import cors from "cors";
 import { errorMiddleware } from "./middleware/err.middleware";
 import codeBrainRoutes from "./routes/codebrain.route";
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use("/api/v1/codebrain", codeBrainRoutes);
 app.use(errorMiddleware);
